bc5ab151288e918ccc3e38cd5e126da0927f911e
jangnguyen <jangnguyen@hotmail.com>
2018-04-21 12:11:19 +0700

    RxJava Operators: Just, From, Range, Repeat

--- app/src/main/java/vn/mb360/rxsample/MainActivity.java
+++ app/src/main/java/vn/mb360/rxsample/MainActivity.java
@@ -10,9 +10,6 @@ import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
-import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 
@@ -33,73 +30,24 @@ public class MainActivity extends AppCompatActivity {
          * RxJava introduction to Operators
          *
          */
-        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-        Observable.fromArray(numbers)
+
+        // Just() => 1 to 10 item emit on observable
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+
+        Observable
+                .fromArray(numbers)
+                .repeat(8)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DisposableObserver<Integer>() {
-                    @Override
-                    public void onNext(Integer integer) {
-                        Log.d(TAG, "onNext: number " + integer);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Log.d(TAG, "onComplete: all number are emitted.");
-                    }
-                });
-
-        // Instead using range
-        Observable.range(1, 20)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DisposableObserver<Integer>() {
-                    @Override
-                    public void onNext(Integer integer) {
-                        Log.d(TAG, "onNext: number " + integer);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Log.d(TAG, "onComplete.");
-                    }
-                });
-
-        // Ví dụ 3: lựa số chẵn, in ra danh sách console "{number} là số chẵn
-        Observable.range(1, 20)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .filter(new Predicate<Integer>() {
-                    @Override
-                    public boolean test(Integer integer) throws Exception {
-                        return integer % 2 == 0;
-                    }
-                })
-                .map(new Function<Integer, String>() {
-                    @Override
-                    public String apply(Integer integer) throws Exception {
-                        return integer + " là số chẵn";
-                    }
-                })
-                .subscribe(new Observer<String>() {
+                .subscribe(new Observer<Integer>() {
                     @Override
                     public void onSubscribe(Disposable d) {
-
+                        // Khong can khoi tao disposable
                     }
 
                     @Override
-                    public void onNext(String s) {
-                        Log.d(TAG, "onNext: " + s);
+                    public void onNext(Integer integer) {
+                        Log.d(TAG, "onNext: " + integer);
                     }
 
                     @Override
